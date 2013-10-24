73d57c625c2602e3af1585f6ced4d7df718cde43
Daouzli <daouzli@gmail.com>
2013-10-24 14:09:14 +0200

    add strip() that removes string's spaces
    add strip() that removes leading & ending spaces characters

---
+++ string_manip.c
@@ -0,0 +1,68 @@
+/** 
+ *  Author: A. Daouzli - Copyright 2013
+ *  Licence: GPL v3
+ *
+ */
+
+void strip(char *s); /** removes external spaces and newline characters from a string */
+
+
+/** Removes starting and ending spaces.
+ *
+ * \param [in|out]s The string to strip.
+ *
+ */
+void strip(char *s)
+{
+    int i;
+    int start = -1, end = -1;
+    int slen = strlen(s);
+
+    /* Find the first non space character */
+
+    for (i = 0; i < slen; i++)
+    {
+        if ( !isspace(s[i]) )
+        {
+            start = i;
+            break;
+        }
+    }
+
+    /* Find the last non space character */
+
+    for (i = slen - 1 ; i >= 0 ; i--)
+    {
+        if ( !isspace(s[i]) )
+        {
+            end = i;
+            break;
+        }
+    }
+    if ( end != -1 )
+    {
+        s[end + 1] = '\0';
+    }
+    else
+    {
+        /* only space characters */
+        if (start == -1)
+        {
+            s[0] = '\0';
+        }
+        else
+        {
+            end = slen - 1;
+        }
+    }
+    /* Move the characters to the left in place of spaces */
+
+    if ( start > 0 )
+    {
+        for ( i = 0 ; i <= (end-start) + 1 ; i++ )
+        {
+            s[i] = s[i + start];
+        }
+    }
+}
+
